a5a41c77315b31cdfdeaee61bed00906f749a652
mr-atuzie <user email>
2024-04-02 11:39:35 +0100

    annoucement

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import Announcement from "./components/Announcement";
 
 function App() {
   return (
     <BrowserRouter>
+      <Announcement />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
