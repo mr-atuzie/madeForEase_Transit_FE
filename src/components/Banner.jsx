e3da0fe201cfc427bc6dc2352f4d973bb6730d42
mr-atuzie <user email>
2024-04-11 08:04:25 +0100

    banner overlay

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className=" w-full h-[80vh]">
+    <div className=" relative w-full h-[80vh]">
       <video className=" w-full h-full object-cover" muted autoPlay loop>
         <source src="https://universaloptions.org/wp-content/uploads/2021/11/Hero-Video.mp4" />
       </video>
+
+      <div className=" absolute top-0 bottom-0 bg-black w-full h-full"></div>
     </div>
   );
 };
