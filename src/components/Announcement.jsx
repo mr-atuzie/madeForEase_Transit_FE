5d0f8571b8dfbfcbbdf3bb2d89303769c2e8f6a4
mr-atuzie <user email>
2024-04-11 08:33:29 +0100

    news slider

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,16 +4,16 @@ import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from "react-icons/md";
 
 const Announcement = () => {
   return (
-    <div className="  font2 flex justify-between bg-black text-white text-sm items-center py-3 px-2">
+    <div className=" absolute bottom-0  font2 flex justify-between bg-black text-white text-sm items-center py-3 px-2">
       <Marquee speed={60} gradient={false} pauseOnHover>
         <div className=" flex gap-6 mr-4">
-          <p className=" font-medium">
+          <p className=" text-xs font-medium">
             USA Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </p>
-          <p className=" font-medium">
+          <p className=" text-xs font-medium">
             Russia Lorem ipsum dolor sit amet consectetur adipisicing.
           </p>
-          <p className="flex items-center font-medium">
+          <p className="flex text-xs items-center font-medium">
             Cryptocurrency Market Cap:{" "}
             <span className=" text-blue-600"> $2.63T </span>
             <span className=" flex text-red-600 ml-2">
@@ -21,7 +21,7 @@ const Announcement = () => {
               1.31%
             </span>
           </p>
-          <p className="flex items-center font-medium">
+          <p className="flex items-center text-xs font-medium">
             24h Vol: <span className=" text-blue-600"> $76.86b </span>
             <span className=" flex text-green-600 ml-2">
               <MdOutlineArrowDropUp />
@@ -30,11 +30,11 @@ const Announcement = () => {
           </p>
         </div>
         <div className=" flex gap-8 mr-4">
-          <p className=" font-medium">
+          <p className=" text-xs font-medium">
             Dominace: <span className=" text-blue-600">BTC: 52.0% </span>{" "}
             <span className=" ml-1 text-blue-600">ETH:16.2%</span>
           </p>
-          <p className=" font-medium">
+          <p className=" text-xs font-medium">
             ETH Gas: <span className=" text-blue-600">19 Gwei </span>{" "}
           </p>
           <div className="flex items-center gap-1">
@@ -43,7 +43,7 @@ const Announcement = () => {
               alt=""
               width={18}
             />
-            <p className=" font-medium">BTC:$69,697.94</p>
+            <p className=" text-xs font-medium">BTC:$69,697.94</p>
           </div>
 
           <div className="flex items-center gap-1">
@@ -52,7 +52,7 @@ const Announcement = () => {
               alt=""
               width={18}
             />
-            <p className=" font-medium">ETH:$3,551.94</p>
+            <p className=" text-xs font-medium">ETH:$3,551.94</p>
           </div>
 
           <div className="flex items-center gap-1">
@@ -61,7 +61,7 @@ const Announcement = () => {
               alt=""
               width={18}
             />
-            <p className=" font-medium">USDT:$1.00</p>
+            <p className=" text-xs font-medium">USDT:$1.00</p>
           </div>
         </div>
       </Marquee>
