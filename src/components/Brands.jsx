a2010ded36d6f92d3836bcb7be0a5dcd8e9d8631
mr-atuzie <user email>
2024-04-13 07:18:02 +0100

    Brands

---
+++ src/components/Brands.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const Brands = () => {
+  const logos = [
+    {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/Mastercard-logo-500x281-min.png",
+    },
+    {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/Oracle-logo-500x281-min.png",
+    },
+    {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/Hermes-Logo-500x284-min.png",
+    },
+    {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/McDonalds-logo-500x281-min.png",
+    },
+    {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/Agricultural-Bank-of-China-logo-500x281-min.png",
+    },
+  ];
+  return (
+    <div>
+      <div className=" w-[90%] mx-auto">
+        <h1 className=" text-center font-extrabold">Our Parteners</h1>
+        <p className=" text-gray-500 text-sm text-center mb-6">
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
+
+        <Marquee speed={70} gradient={false} pauseOnHover>
+          <div className=" grid gap-4 grid-cols-4">
+            {logos.map((logo, index) => {
+              return <img className=" w-20" key={index} src={logo} alt="" />;
+            })}
+          </div>
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default Brands;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
+import Brands from "../components/Brands";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
-
+      <Brands />
       <Footer />
     </div>
   );
