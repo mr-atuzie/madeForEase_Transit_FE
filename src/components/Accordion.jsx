31e2d77d9acb6de1ca7fa78e9d6da3f8afe8bd95
mr-atuzie <user email>
2024-04-14 12:44:53 +0100

    accordion

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa6";
+
+const Accordion = ({ title, answer }) => {
+  const [accordion, setAccordion] = useState(false);
+  return (
+    <div className=" py-2">
+      <button
+        className=" w-full flex justify-between items-center"
+        onClick={() => setAccordion(!accordion)}
+      >
+        <h1>{title}</h1>
+
+        {accordion ? (
+          <span className=" text-orange-500">
+            <FaMinus />
+          </span>
+        ) : (
+          <span className=" text-orange-500">
+            <FaPlus />
+          </span>
+        )}
+      </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+          accordion
+            ? " grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0"
+        }`}
+      >
+        <p>{answer}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import Accordion from "./Accordion";
 
 const Faq = () => {
   return (
     <div className=" font2 py-4">
       <div className=" w-[90%] mx-auto">
         <h1 className=" text-center font-extrabold">FAQ</h1>
-        <p className=" text-gray-500 text-sm text-center mb-6">
-          Lorem ipsum dolor sit amet consectetur.
+        <p className=" text-gray-500 capitalize text-sm text-center mb-6">
+          frequently asked questions
         </p>
+
+        <Accordion
+          title="Do you really love your girlfriend?"
+          answer="Depend on the mood you fins me,I can lie she is wonderful and all but most times she's a BITCH."
+        />
+
+        <Accordion
+          title="Ass or Boobs?"
+          answer="Depend on the mood you fins me,I can lie she is wonderful and all but most times she's a BITCH."
+        />
+
+        <Accordion
+          title="Would you take 1 million naira or a Night with Ayra star?"
+          answer="Depend on the mood you fins me,I can lie she is wonderful and all but most times she's a BITCH."
+        />
+
+        <Accordion
+          title="J cole, Kendrick lamar or Drake?"
+          answer="Depend on the mood you fins me,I can lie she is wonderful and all but most times she's a BITCH."
+        />
       </div>
     </div>
   );
