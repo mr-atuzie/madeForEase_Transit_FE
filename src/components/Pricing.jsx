8e930c2f652ea77a2561dba823da2a678b0df002
mr-atuzie <user email>
2024-04-14 14:10:17 +0100

    /--

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,7 @@ const Pricing = () => {
   return (
     <div className=" font2 py-8">
       <div className=" w-[90%] mx-auto">
-        <h1 className="  text-2xl text-center font-extrabold mb-3">
+        <h1 className="  text-2xl text-center font-extrabold mb-6">
           Our Packages
         </h1>
         <div className=" grid gap-6 grid-cols-1 md:grid-cols-4 justify-between">
