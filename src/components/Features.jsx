c817042ce76af2d2040ff8a90e1c28fef6a60ac2
rex atuzie <rexatuzie@gmail.com>
2024-12-16 13:56:31 +0100

    features -- 3 cards

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaRegCalendarAlt } from "react-icons/fa";
+import { FaPiggyBank } from "react-icons/fa6";
 
 const Features = () => {
   return (
@@ -11,14 +12,38 @@ const Features = () => {
 
       <div className=" grid grid-cols-1 lg:grid-cols-3">
         <div className=" p-3 flex flex-col gap-3  shadow-sm">
-          <span className=" w-14 h-14 rounded-lg flex justify-center items-center bg-yellow-100">
+          <span className=" w-14 h-14 rounded-lg flex justify-center items-center bg-yellow-100 text-yellow-600">
             <FaMapMarkerAlt size={20} />
           </span>
 
-          <h1>Real-time Tracking</h1>
-          <p>
+          <h1 className=" font-semibold">Real-time Tracking</h1>
+          <p className=" text-sm text-gray-600">
             Know exactly where your ride is in real-time. Stay updated and avoid
             unnecessary wait times.
+          </p>
+        </div>
+
+        <div className=" p-3 flex flex-col gap-3  shadow-sm">
+          <span className=" w-14 h-14 rounded-lg flex justify-center items-center bg-yellow-100 text-yellow-600">
+            <FaRegCalendarAlt size={20} />
+          </span>
+
+          <h1 className=" font-semibold">Flexible Scheduling</h1>
+          <p className=" text-sm text-gray-600">
+            Plan trips effortlessly with customizable schedules that fit your
+            lifestyle.
+          </p>
+        </div>
+
+        <div className=" p-3 flex flex-col gap-3  shadow-sm">
+          <span className=" w-14 h-14 rounded-lg flex justify-center items-center bg-yellow-100 text-yellow-600">
+            <FaPiggyBank size={20} />
+          </span>
+
+          <h1 className=" font-semibold">Affordable Pricing</h1>
+          <p className=" text-sm text-gray-600">
+            Save money on your daily commute with budget-friendly plans tailored
+            for everyone
           </p>
         </div>
       </div>
