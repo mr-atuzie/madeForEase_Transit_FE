2c63f88f10f96601e39311a30761fa675dc3c352
rex atuzie <rexatuzie@gmail.com>
2024-12-16 06:54:33 +0100

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
-import Faq from "../components/Faq";
+// import Faq from "../components/Faq";
 import Reviews from "../components/Reviews";
 
 const Home = () => {
@@ -10,16 +10,16 @@ const Home = () => {
     <div>
       <Navbar />
       <Banner />
-      <div className=" font2">
+      {/* <div className=" font2">
         <div className=" w-[90%] mx-auto">
           <h1 className=" text-center font-extrabold">About Us</h1>
           <p className=" text-gray-500 text-sm text-center mb-6">
             Lorem ipsum dolor sit amet consectetur.
           </p>
         </div>
-      </div>
+      </div> */}
       <Reviews />
-      <Faq />
+      {/* <Faq /> */}
       <Footer />
     </div>
   );
