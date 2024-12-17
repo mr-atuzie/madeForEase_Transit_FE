9d059611539a6c59c65afeb811b69d247130731d
rex atuzie <rexatuzie@gmail.com>
2024-12-17 09:06:45 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -65,21 +65,21 @@ const Footer = () => {
     <footer className=" bg-black text-white   py-10 flex justify-center items-center font2">
       <div className=" w-[90%] mx-auto ">
         <div className="lg:flex justify-between">
-          <h3 className=" text-sm lg:w-[15%] mb-8 lg:mb-0 text-yellow-500 uppercase ">
+          <h3 className=" text-sm font-semibold lg:w-[15%] mb-8 lg:mb-0 text-yellow-600 uppercase ">
             made for ease transit
           </h3>
           <div className="lg: w-[80%] flex gap-6 flex-col lg:flex-row justify-between">
             {footer.map((item) => {
               return (
                 <div key={item.title}>
-                  <h2 className=" text-yellow-600  font-semibold mb-4">
+                  <h2 className=" text-sm lg:text-base text-yellow-600  font-semibold mb-4">
                     {item.title}
                   </h2>
 
                   {item.list.map((l, index) => (
                     <p
                       key={index}
-                      className=" text-sm text-gray-500 font-medium mb-2.5 hover:text-black hover:font-semibold"
+                      className=" text-sm text-gray-200 font-medium mb-2.5 hover:text-yellow-500 hover:font-semibold"
                     >
                       {l}
                     </p>
@@ -90,7 +90,7 @@ const Footer = () => {
           </div>
         </div>
         <div className=" border-t mt-5 lg:mt-10">
-          <p className=" text-center my-4 text-sm lg:text-base">
+          <p className=" text-center my-4 text-xs text-yellow-500 lg:text-base">
             made for ease transit &copy; 2024.{" "}
             <span className=" font-medium">All rights reserved</span>{" "}
           </p>
